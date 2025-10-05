51e0f05c7c63dcc5b5dc2362e36aadd0fb9e7109
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-05 13:42:03 +0300

    creating a register view

--- users/views.py
+++ users/views.py
@@ -1,8 +1,37 @@
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status, generics
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .serializers import RegisterSerializer, LoginSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    queryset = User.objects.all()
+
+    def create(self,request,*args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                "user":{
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "phone": user.phone
+                },
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },status=status.HTTP_201_CREATED,
+        )
