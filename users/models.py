88f46b7c82e5b0d125c9eacea46b3bd589771708
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-05 11:40:24 +0300

    updating the user model

--- users/models.py
+++ users/models.py
@@ -6,6 +6,10 @@ import uuid
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, editable=False,default=uuid.uuid4)
     phone = models.CharField(max_length=10, unique=True, null=True, blank=True)
+    email = models.EmailField(unique=True)
+
+    # USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email','phone']
 
     def __str__(self):
         return self.username
