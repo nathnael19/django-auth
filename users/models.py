ea87b857913773c77c217ae0a17874539a2f096b
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-05 10:28:01 +0300

    creating and registering user model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+# Create your models here.
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, editable=False,default=uuid.uuid4)
+    phone = models.CharField(max_length=10, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
