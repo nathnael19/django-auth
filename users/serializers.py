e1be648e6515d7472c08852c8c39b731aa53c383
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-05 13:35:42 +0300

    creating a login serializer

--- users/serializers.py
+++ users/serializers.py
@@ -35,3 +35,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
     
 
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+
+        user = self.authenticate(email=email,password=password)
+
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+        data['user'] = user
+        return data
