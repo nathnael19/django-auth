d198958ab9add07d41c6ee5d72b0f4f0f586b23b
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-10-11 09:39:14 +0300

    ready for deployment

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cs^6=qe)6r7@pf4_q-p2(zm&jy#j@$!24zat@ndr6&1+li3+id
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -127,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
